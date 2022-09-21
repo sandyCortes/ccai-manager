26a7d4bf30f2fb738d7693f639fb65f639ffb634
Melinna <hcym050397@hotmail.com>
2022-09-21 11:44:04 -0500

    componente card

--- src/components/ui/Cards/index.js
+++ src/components/ui/Cards/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
+
+
 export const CardComponent = () => {
   return (
     <>
-      <div>Hola Mundo...!</div>
+    
+<div class="card">
+
+ <card class="card card-radious"/>   
+ <header class="card-header">
+    
+  
+    <p class="card-header-title">
+      Registro Alumno 
+    </p>
+    
+    
+  </header>
+    <div class="card">
+    <card class="card card-radious"/> 
+    <div class="card-image">
+    <figure class="image is-4by3">
+        
+    </figure>
+  </div>
+</div>
+</div> 
+    
+      
+
     </>
   )
 }
