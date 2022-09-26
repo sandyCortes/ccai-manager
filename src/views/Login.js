93195760030b5de46e0c96fe6fa5802b90a3d9b5
Sandra Cortes <sandra_hakufu@hotmail.com>
2022-09-26 11:53:27 -0500

    change file names

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,11 +1,11 @@
 import React from 'react';
 // import { InputLabel } from '../components/ui/Inputs/InputLabel'
-import { CardComponent } from '../components/ui/Cards'
+import { CardComponent } from '../components/ui/Cards/CardComponent'
 
 const Login = () => {
   return (
     <div className='container is-widescreen'>
-      <CardComponent />
+      Hola
     </div>
   )
 }
