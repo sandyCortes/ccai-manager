1533180a2942dd1d6e3fe8c0604b445a927f4261
ulises <ulises-garcia2@hotmail.com>
2022-10-03 16:56:02 -0500

    Se agrego la pantalla de registro

--- src/views/Register.js
+++ src/views/Register.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import { CardComponent } from './../components/ui/Cards/CardComponent'
 import { InputLabel } from './../components/ui/Inputs/InputLabel'
+import { ButtonComponent } from '../components/ui/Buttons/PrimaryButton'
+import { HeaderComponent } from './../components/ui/Header/HeaderComponent' 
 
 const Register = () => {
    return (
-    <div className='container'>
-      <CardComponent classExtra="opacity-card">
-        <div>Hola Mundo, aqui te registras...!!</div>
-        <InputLabel title="Nombre" />
+    <div className='container register-content'>
+       <HeaderComponent title="Registro"/>
+       <CardComponent classExtra="opacity-card margin-top">
+        <div className='title-register'>DATOS PERSONALES</div>
+        <div className = "container-personal">     
+         <div class="columns">
+            <div class="column is-3">
+             <InputLabel title="Nombre" />
+            </div>
+            <div class="column is-3">
+             <InputLabel title="Nombre" />
+            </div>
+            <div class="column is-3">
+             <InputLabel title="Nombre" />
+            </div>
+            <div class="column is-3">
+             <InputLabel title="Nombre" />
+            </div>
+            <div class="column is-3">
+             <InputLabel title="Nombre" />
+            </div>
+            <div class="column is-3">
+             <InputLabel title="Nombre" />
+            </div>
+          </div>
+        </div>
+         
       </CardComponent>
      </div>
    )
