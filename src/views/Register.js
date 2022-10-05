6fea08c6b87dc90f02f9641d2f00d604dec782a1
ulises <ulises-garcia2@hotmail.com>
2022-10-05 10:33:58 -0500

    Actualizacion oantalla de registro

--- src/views/Register.js
+++ src/views/Register.js
@@ -9,31 +9,51 @@ const Register = () => {
     <div className='container register-content'>
        <HeaderComponent title="Registro"/>
        <CardComponent classExtra="opacity-card margin-top">
-        <div className = "container-personal">
-          <div class="columns">
-            <div className='column is-12 title-register'>DATOS PERSONALES</div>
+        <div className='title-register'>DATOS PERSONALES</div>
+          <div className = "container-personal ">     
+           <div class="columns">
               <div class="column is-3">
                 <InputLabel title="Nombre" />
               </div>
-              <div class="column is-3">
-                <InputLabel title="Nombre" />
-              </div>
-              <div class="column is-3">
-                <InputLabel title="Nombre" />
-              </div>
-              <div class="column is-3">
-                <InputLabel title="Nombre" />
-              </div>
-              <div class="column is-3">
-                <InputLabel title="Nombre" />
-              </div>
-              <div class="column is-3">
-                <InputLabel title="Nombre" />
-              </div>
-
-            <div className='column is-12 title-register'>
-              DATOS Inst
+                <div class="column is-3">
+                 <InputLabel title="Apellido paterno" />
+                </div>
+            <div class="column is-3">
+             <InputLabel title="Apellido materno" />
             </div>
+        </div>    
+            <div class="columns">
+              <div class="column is-3">
+                <InputLabel title="Dirección" />
+              </div>
+                <div class="column is-3">
+                  <InputLabel title="Telefono" />
+                </div>
+                  <div class="column is-3">
+                    <InputLabel title="Sexo" />
+                  </div>
+          </div>
+        </div>
+        <div className='title-register '>DATOS INSTITUCIONALES</div>
+        <div className = "container-personal ">     
+           <div class="columns">
+              <div class="column is-3">
+                <InputLabel title="Matricula" />
+              </div>
+                <div class="column is-3">
+                 <InputLabel title="Carrera" />
+                </div>
+            <div class="column is-3">
+             <InputLabel title="Servicio a prestar" />
+            </div>
+        </div>    
+            <div class="columns">
+              <div class="column is-3">
+                <InputLabel title="Correo institucional" />
+              </div>
+                <div class="column is-3">
+                  <InputLabel title="Contraseña" />
+                </div>
           </div>
         </div>
       </CardComponent>
