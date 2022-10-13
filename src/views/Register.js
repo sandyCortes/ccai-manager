0fdafa9f4fb12330d63527e79549f5b23e01f1e1
Sandra Cortes <sandra_hakufu@hotmail.com>
2022-10-13 14:50:07 -0500

    update styles in card

--- src/views/Register.js
+++ src/views/Register.js
@@ -7,66 +7,58 @@ const Register = () => {
    return (
     <div className='container register-content'>
       <HeaderComponent title="Registro"/>
-        <CardComponent classExtra="margin-top opacity-card">
-          <div style={{ margin: '2%' }}>
-            <div className='title-register'>DATOS PERSONALES</div>
+      <CardComponent classExtra="opacity-card">
+        <div className="columns container-personal">
+          <div className="column is-11">
+            <p className='title-register'>DATOS PERSONALES</p>
           </div>
-            <div className = "container-personal ">     
-              <div className="columns">
-                <div className="column is-3">
-                  <InputLabel title="Nombre" />
-                </div>
-                  <div className="column is-3">
-                    <InputLabel title="Apellido paterno" />
-                  </div>
-                    <div className="column is-3">
-                      <InputLabel title="Apellido materno" />
-                    </div>
-              </div>    
-              <div className="columns">
-                <div className="column is-3">
-                  <InputLabel title="Dirección" />
-                </div>
-                  <div className="column is-3">
-                    <InputLabel title="Telefono" />
-                  </div>
-                    <div className="column is-3">
-                     <InputLabel title="Sexo" />
-                    </div>
-              </div>
-            </div>
-          <div style={{ margin: '2%' }}>
-            <div className='title-register'>DATOS INSTITUCIONALES</div>
+          <div className="column is-4">
+            <InputLabel title="Nombre" />
           </div>
-            <div className = 'container-personal'>     
-              <div className="columns">
-            <div className="column is-3">
-              <InputLabel title="Matricula" />
-            </div>
-              <div className="column is-3">
-                <InputLabel title="Carrera" />
-              </div>
-            <div className="column is-3">
-             <InputLabel title="Servicio a prestar" />
-            </div>
-          </div>    
-            <div className="columns">
-              <div className="column is-3">
-                <InputLabel title="Correo institucional" />
-              </div>
-                <div className="column is-3">
-                  <InputLabel title="Contraseña" />
-                </div>
-                <div className="column is-3">
-                  <p className="control has-icon-right">
-                    <button className="button button-register">
-                      <span className="icon is-right">
-                        <i className="mdi mdi-plus-circle-outline"></i>
-                      </span>  
-                    </button>
-                  </p>
-                </div>
-            </div>
+          <div className="column is-4">
+            <InputLabel title="Apellido paterno" />
+          </div>
+          <div className="column is-4">
+            <InputLabel title="Apellido materno" />
+          </div>
+          <div className="column is-4">
+            <InputLabel title="Dirección" />
+          </div>
+          <div className="column is-4">
+            <InputLabel title="Telefono" />
+          </div>
+          <div className="column is-4">
+            <InputLabel title="Sexo" />
+          </div>
+
+          <div className="column is-11">
+            <p className='title-register'>DATOS INSTITUCIONALES</p>
+          </div>
+
+          <div className="column is-4">
+            <InputLabel title="Matricula" />
+          </div>
+          <div className="column is-4">
+            <InputLabel title="Carrera" />
+          </div>
+          <div className="column is-4">
+            <InputLabel title="Servicio a prestar" />
+          </div>
+          <div className="column is-4">
+            <InputLabel title="Correo institucional" />
+          </div>
+          <div className="column is-4">
+            <InputLabel title="Contraseña" />
+          </div>
+          <div className="column is-4">
+            <p className="control has-icon-right">
+              <button className="button button-register">
+                <span className="icon is-right">
+                  <i className="mdi mdi-plus-circle-outline"></i>
+                </span>  
+              </button>
+            </p>
+          </div>
         </div>
       </CardComponent>
     </div>
